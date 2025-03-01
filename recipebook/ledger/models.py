a5cbc5f3f5507852cda6347fa9c6b6316817aff1
tobi <tobey.gaerlan@student.ateneo.edu>
2025-03-01 23:16:01 +0800

    adding models n reading docu poorly

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+class Recipe(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class RecipeIngredient(models.Model):
+    quantity = models.CharField(max_length=50)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
