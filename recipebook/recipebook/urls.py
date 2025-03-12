6898634f0352e29cc3c2af1d85586446d06f8002
tobi <tobey.gaerlan@student.ateneo.edu>
2025-03-12 16:28:05 +0800

    init commit (anything added from class)

--- recipebook/recipebook/urls.py
+++ recipebook/recipebook/urls.py
@@ -19,5 +19,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ledger/', include('ledger.urls'))
+    path('ledger/', include('ledger.urls')),
+    path('accounts/', include('django.contrib.auth.urls'))
+
 ]
