1b523ee1c56c04dce3db891d6da0f8f5ed30c34a
tobi <tobey.gaerlan@student.ateneo.edu>
2025-03-13 10:16:17 +0800

    added recipe author, created_on, and updated_on fields

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -12,6 +12,9 @@ class Ingredient(models.Model):
     
 class Recipe(models.Model):
     name = models.CharField(max_length=50)
+    author = models.CharField(null=True, max_length=50)
+    created_on = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_on = models.DateTimeField(auto_now=True, editable=False)
 
     def __str__(self):
         return self.name
